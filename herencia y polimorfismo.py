e4a48ebce2a9352b86398fe22a4fdf818d02a48f
Cristian Cordero <cristiancordero2810@gmail.com>
2025-07-20 10:21:45 -0500

    primer ejercicio entendiendo el concepto de herencia y polimorfismo

--- herencia y polimorfismo.py
+++ herencia y polimorfismo.py
@@ -1,3 +1,37 @@
 """
-Herencia
+Ejercicio de herencia y polimorfismo en Python.
 """
+
+#Superclase
+
+class Animal:
+    def __init__ (self, name: str):
+        self.name = name 
+    
+    def sound(self):
+        pass    # Método que será sobreescrito por las subclases 
+
+#Subclases
+
+class Cat(Animal):
+    def sound(self):
+        print ("Meow!")
+
+class Dog(Animal):
+    def sound(self):
+        print ("Woof!")
+    def specie(self):
+        return "lobo ciberiano"
+    
+    
+def print_sound(animal: Animal):
+    animal.sound()
+
+my_pet = Animal("Generic Animal")
+my_pet.sound()    
+my_cat = Cat("Pelusa")
+#print(my_cat.name, "hace:", my_cat.sound()) --> funciona con return
+print_sound(my_cat) # eliminar esta linea si usa return en lugar de print en la función
+my_dog = Dog("Firulais") 
+#print(my_dog.name, "hace:", my_dog.sound(), "y es un", my_dog.specie()) --> funciona con return
+print_sound(my_dog) # eliminar esta linea si usa return en lugar de print en la función
